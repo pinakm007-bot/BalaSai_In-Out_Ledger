165118ab80b070f9a4e1f77a3152765bd8b41e53
Geo-Prana <pinakm007@gmail.com>
2026-09-04 18:27:22 +0530

    Delete app directory

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-
-export const metadata = { title: 'Bala Sai Ledger Portal', description: 'Daily Purchase & Payment Entry' };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{margin:0, fontFamily:'Inter, system-ui'}}>{children}</body>
-    </html>
-  );
-}
